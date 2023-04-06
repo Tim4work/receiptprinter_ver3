38edf1878370ff07828be8fe95f170caba7b49af
kjw <yoonlight12@gmail.com>
2023-04-07 01:14:58 +0900

    feat: add ecs pos printer

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'dart:io';
 import 'dart:async';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
+import 'package:flutter/services.dart';
+import 'package:oktoast/oktoast.dart';
+import 'package:intl/intl.dart';
+import 'package:image/image.dart' as img;
 
 List<CameraDescription>? cameras;
 Future<void> main() async {
@@ -15,13 +20,14 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return OKToast(
+        child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    ));
   }
 }
 
@@ -35,31 +41,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   CameraController? controller;
   String imagePath = "";
-  final flutterReactiveBle = FlutterReactiveBle();
-  FlutterReactiveBle get _ble => flutterReactiveBle;
-  final List<DiscoveredDevice> _devices = [];
-  bool _scanStarted = false;
-  late StreamSubscription<DiscoveredDevice> _scanStream;
-
-  scan() async {
-    setState(() {
-      _scanStarted = true;
-    });
-    _devices.clear();
-    _scanStream = _ble.scanForDevices(
-        withServices: [], scanMode: ScanMode.lowLatency).listen((device) {
-      final knownDeviceIndex = _devices.indexWhere((d) => d.id == device.id);
-      if (knownDeviceIndex >= 0) {
-        _devices[knownDeviceIndex] = device;
-      } else {
-        _devices.add(device);
-        setState(() {});
-      }
-    });
-    _ble.statusStream.listen((event) {
-      debugPrint(event.toString());
-    });
-  }
+  PrinterBluetoothManager printerManager = PrinterBluetoothManager();
+  List<PrinterBluetooth> _devices = [];
 
   @override
   void initState() {
@@ -69,7 +52,12 @@ class _MyHomePageState extends State<MyHomePage> {
       if (!mounted) {
         return;
       }
-      setState(() {});
+      printerManager.scanResults.listen((devices) async {
+        // print('UI: Devices found ${devices.length}');
+        setState(() {
+          _devices = devices;
+        });
+      });
     });
   }
 
@@ -77,6 +65,284 @@ class _MyHomePageState extends State<MyHomePage> {
   void dispose() {
     controller?.dispose();
     super.dispose();
+  }
+
+  void _startScanDevices() {
+    setState(() {
+      _devices = [];
+    });
+    printerManager.startScan(const Duration(seconds: 4));
+  }
+
+  void _stopScanDevices() {
+    printerManager.stopScan();
+  }
+
+  Future<List<int>> demoReceipt(
+      PaperSize paper, CapabilityProfile profile) async {
+    final Generator ticket = Generator(paper, profile);
+    List<int> bytes = [];
+
+    // Print image
+    // final ByteData data = await rootBundle.load('assets/rabbit_black.jpg');
+    // final Uint8List imageBytes = data.buffer.asUint8List();
+    // final Image? image = decodeImage(imageBytes);
+    // bytes += ticket.image(image);
+
+    bytes += ticket.text('GROCERYLY',
+        styles: const PosStyles(
+          align: PosAlign.center,
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ),
+        linesAfter: 1);
+
+    bytes += ticket.text('889  Watson Lane',
+        styles: const PosStyles(align: PosAlign.center));
+    bytes += ticket.text('New Braunfels, TX',
+        styles: const PosStyles(align: PosAlign.center));
+    bytes += ticket.text('Tel: 830-221-1234',
+        styles: const PosStyles(align: PosAlign.center));
+    bytes += ticket.text('Web: www.example.com',
+        styles: const PosStyles(align: PosAlign.center), linesAfter: 1);
+
+    bytes += ticket.hr();
+    bytes += ticket.row([
+      PosColumn(text: 'Qty', width: 1),
+      PosColumn(text: 'Item', width: 7),
+      PosColumn(
+          text: 'Price',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: 'Total',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+    ]);
+
+    bytes += ticket.row([
+      PosColumn(text: '2', width: 1),
+      PosColumn(text: 'ONION RINGS', width: 7),
+      PosColumn(
+          text: '0.99',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '1.98',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+    ]);
+    bytes += ticket.row([
+      PosColumn(text: '1', width: 1),
+      PosColumn(text: 'PIZZA', width: 7),
+      PosColumn(
+          text: '3.45',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '3.45',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+    ]);
+    bytes += ticket.row([
+      PosColumn(text: '1', width: 1),
+      PosColumn(text: 'SPRING ROLLS', width: 7),
+      PosColumn(
+          text: '2.99',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '2.99',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+    ]);
+    bytes += ticket.row([
+      PosColumn(text: '3', width: 1),
+      PosColumn(text: 'CRUNCHY STICKS', width: 7),
+      PosColumn(
+          text: '0.85',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '2.55',
+          width: 2,
+          styles: const PosStyles(align: PosAlign.right)),
+    ]);
+    bytes += ticket.hr();
+
+    bytes += ticket.row([
+      PosColumn(
+          text: 'TOTAL',
+          width: 6,
+          styles: const PosStyles(
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
+      PosColumn(
+          text: '\$10.97',
+          width: 6,
+          styles: const PosStyles(
+            align: PosAlign.right,
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
+    ]);
+
+    bytes += ticket.hr(ch: '=', linesAfter: 1);
+
+    bytes += ticket.row([
+      PosColumn(
+          text: 'Cash',
+          width: 7,
+          styles:
+              const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+      PosColumn(
+          text: '\$15.00',
+          width: 5,
+          styles:
+              const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    ]);
+    bytes += ticket.row([
+      PosColumn(
+          text: 'Change',
+          width: 7,
+          styles:
+              const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+      PosColumn(
+          text: '\$4.03',
+          width: 5,
+          styles:
+              const PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    ]);
+
+    bytes += ticket.feed(2);
+    bytes += ticket.text('Thank you!',
+        styles: const PosStyles(align: PosAlign.center, bold: true));
+
+    final now = DateTime.now();
+    final formatter = DateFormat('MM/dd/yyyy H:m');
+    final String timestamp = formatter.format(now);
+    bytes += ticket.text(timestamp,
+        styles: const PosStyles(align: PosAlign.center), linesAfter: 2);
+
+    // Print QR Code from image
+    // try {
+    //   const String qrData = 'example.com';
+    //   const double qrSize = 200;
+    //   final uiImg = await QrPainter(
+    //     data: qrData,
+    //     version: QrVersions.auto,
+    //     gapless: false,
+    //   ).toImageData(qrSize);
+    //   final dir = await getTemporaryDirectory();
+    //   final pathName = '${dir.path}/qr_tmp.png';
+    //   final qrFile = File(pathName);
+    //   final imgFile = await qrFile.writeAsBytes(uiImg.buffer.asUint8List());
+    //   final img = decodeImage(imgFile.readAsBytesSync());
+
+    //   bytes += ticket.image(img);
+    // } catch (e) {
+    //   print(e);
+    // }
+
+    // Print QR Code using native function
+    // bytes += ticket.qrcode('example.com');
+
+    ticket.feed(2);
+    ticket.cut();
+    return bytes;
+  }
+
+  Future<List<int>> testTicket(
+      PaperSize paper, CapabilityProfile profile) async {
+    final Generator generator = Generator(paper, profile);
+    List<int> bytes = [];
+
+    bytes += generator.text(
+        'Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
+    // bytes += generator.text('Special 1: àÀ èÈ éÉ ûÛ üÜ çÇ ôÔ',
+    //     styles: PosStyles(codeTable: PosCodeTable.westEur));
+    // bytes += generator.text('Special 2: blåbærgrød',
+    //     styles: PosStyles(codeTable: PosCodeTable.westEur));
+
+    bytes += generator.text('Bold text', styles: const PosStyles(bold: true));
+    bytes +=
+        generator.text('Reverse text', styles: const PosStyles(reverse: true));
+    bytes += generator.text('Underlined text',
+        styles: const PosStyles(underline: true), linesAfter: 1);
+    bytes += generator.text('Align left',
+        styles: const PosStyles(align: PosAlign.left));
+    bytes += generator.text('Align center',
+        styles: const PosStyles(align: PosAlign.center));
+    bytes += generator.text('Align right',
+        styles: const PosStyles(align: PosAlign.right), linesAfter: 1);
+
+    bytes += generator.row([
+      PosColumn(
+        text: 'col3',
+        width: 3,
+        styles: const PosStyles(align: PosAlign.center, underline: true),
+      ),
+      PosColumn(
+        text: 'col6',
+        width: 6,
+        styles: const PosStyles(align: PosAlign.center, underline: true),
+      ),
+      PosColumn(
+        text: 'col3',
+        width: 3,
+        styles: const PosStyles(align: PosAlign.center, underline: true),
+      ),
+    ]);
+
+    bytes += generator.text('Text size 200%',
+        styles: const PosStyles(
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ));
+
+    // Print image
+    final ByteData data = await rootBundle.load('assets/logo.png');
+    final Uint8List buf = data.buffer.asUint8List();
+    final img.Image image = img.decodeImage(buf)!;
+    bytes += generator.image(image);
+    // Print image using alternative commands
+    // bytes += generator.imageRaster(image);
+    // bytes += generator.imageRaster(image, imageFn: PosImageFn.graphics);
+
+    // Print barcode
+    final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
+    bytes += generator.barcode(Barcode.upcA(barData));
+
+    // Print mixed (chinese + latin) text. Only for printers supporting Kanji mode
+    // bytes += generator.text(
+    //   'hello ! 中文字 # world @ éphémère &',
+    //   styles: PosStyles(codeTable: PosCodeTable.westEur),
+    //   containsChinese: true,
+    // );
+
+    bytes += generator.feed(2);
+
+    bytes += generator.cut();
+    return bytes;
+  }
+
+  void _testPrint(PrinterBluetooth printer) async {
+    printerManager.selectPrinter(printer);
+
+    // TODO Don't forget to choose printer's paper
+    const PaperSize paper = PaperSize.mm80;
+    final profile = await CapabilityProfile.load();
+
+    // TEST PRINT
+    // final PosPrintResult res =
+    // await printerManager.printTicket(await testTicket(paper));
+
+    // DEMO RECEIPT
+    final PosPrintResult res =
+        await printerManager.printTicket((await demoReceipt(paper, profile)));
+
+    showToast(res.msg);
   }
 
   @override
@@ -123,23 +389,66 @@ class _MyHomePageState extends State<MyHomePage> {
                     File(imagePath),
                   )),
             if (_devices.isNotEmpty)
-              ListView(
-                shrinkWrap: true,
-                children: [
-                  for (var i = 0; i < _devices.length; i++)
-                    ListTile(title: Text(_devices[i].name))
-                ],
-              )
+              ListView.builder(
+                  itemCount: _devices.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () => _testPrint(_devices[index]),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 60,
+                            padding: const EdgeInsets.only(left: 10),
+                            alignment: Alignment.centerLeft,
+                            child: Row(
+                              children: <Widget>[
+                                const Icon(Icons.print),
+                                const SizedBox(width: 10),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(_devices[index].name ?? ''),
+                                      Text(_devices[index].address!),
+                                      Text(
+                                        'Click to print a test receipt',
+                                        style:
+                                            TextStyle(color: Colors.grey[700]),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const Divider(),
+                        ],
+                      ),
+                    );
+                  }),
           ]),
         ),
       ),
-      persistentFooterButtons: [
-        ElevatedButton(
-            onPressed: () {
-              scan();
-            },
-            child: const Text("스캔"))
-      ],
+      floatingActionButton: StreamBuilder<bool>(
+        stream: printerManager.isScanningStream,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data!) {
+            return FloatingActionButton(
+              onPressed: _stopScanDevices,
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.stop),
+            );
+          } else {
+            return FloatingActionButton(
+              onPressed: _startScanDevices,
+              child: const Icon(Icons.search),
+            );
+          }
+        },
+      ),
     );
   }
 }
